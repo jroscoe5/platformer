3656b89d7375d1a3c1e7bee554fadde3aeaa0a2b
Bridgerz <bridgerzoske@gmail.com>
2018-11-03 16:09:48 -0700

    changes

--- Platformer/Assets/Scripts/PlayerManager.cs
+++ Platformer/Assets/Scripts/PlayerManager.cs
@@ -7,10 +7,14 @@ public class PlayerManager : MonoBehaviour {
     public int deathCount;
     public int Score;
     public Vector3 spawnPos;
+    public GameObject Player;
 
 	// Use this for initialization
 	void Start ()
     {
+        Player.SetActive(false);
+        Player.transform.position = spawnPos;
+        Player.SetActive(true);
         deathCount = 0;
         Score = 0;
 	}
